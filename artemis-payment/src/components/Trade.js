e93413adf6dbc7b6329d1c568d747fe0600ded48
Millad Ayoubi <Milladayoubi@gmail.com>
2023-06-01 00:44:10 -0400

    Update

--- artemis-payment/src/components/Trade.js
+++ artemis-payment/src/components/Trade.js
@@ -12,13 +12,13 @@ const Trade = () => {
   
     <div className='container mx-auto'>
       <h2 className='section-title text-center mb-16'>What people are saying about us</h2>
-      <div className='flex flex-col gap-[45px] lg:flex-row' data-aos="fade-up" data-aos-delay="900"data-aos-duration="1200">
+      <div className='flex flex-col gap-[45px] lg:flex-row ' data-aos="fade-up" data-aos-delay="900"data-aos-duration="1200" >
         {reviews.map((item, index)=> {
 
           const {image, name, position, description} = item;
           return (
           
-          <div className='w-full rounded full py-12 px-6 shadow-primary cursor-pointer transition duration 300' key={index} >
+          <div className='w-full  py-12 px-6 shadow-primary cursor-pointer  buissness-icon-container' key={index} >
             
           <div className='flex flex-col justify-center'>
            <div>
